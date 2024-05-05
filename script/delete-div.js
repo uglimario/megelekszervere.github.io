8e568bc9d0b4ddbd674c243f15ce8a7289d98f6c
uglimario <bs.uglimario@gmail.com>
2024-05-05 20:53:46 +0200

    Megelek music + footer

---
+++ script/delete-div.js
@@ -0,0 +1,4 @@
+while (true) {
+    const element = getElementsByTagName("div"); boxes[0]. className = "elfsight-app-516ae77f-6703-4cac-b4a9-961a64f9f5f2";
+    element.remove(); // Removes the div with the 'div-02' id
+}
